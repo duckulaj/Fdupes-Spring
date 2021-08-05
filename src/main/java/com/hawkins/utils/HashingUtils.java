72e1d02747cb4a2b1193b2c3c0ab1f60ac1c5563
jonathan <jonathan@CM-AMD-LINUX>
2021-08-05 10:42:23 +0100

    Add SHA256

---
+++ src/main/java/com/hawkins/utils/HashingUtils.java
@@ -0,0 +1,28 @@
+package com.hawkins.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.github.cbismuth.fdupes.container.immutable.PathElement;
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
+
+public class HashingUtils {
+
+
+	public static String md5(PathElement file) {
+		try {
+			return Files.asByteSource(file.getPath().toFile()).hash(Hashing.md5()).toString();
+		} catch (Exception e) {
+			throw new RuntimeException(String.format("Failed to calculate md5 of %s", file));
+		}
+	}	
+
+	public static String shaSum256(PathElement file) {
+		try {
+			return Files.asByteSource(file.getPath().toFile()).hash(Hashing.sha256()).toString();
+		} catch (IOException e) {
+			throw new RuntimeException(String.format("Failed to calculate shaSum256 of %s", file));
+		}
+	}
+}
